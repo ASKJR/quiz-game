a6b51520849f6ab7498cc3abbe0df1b00397b11a
ASKJR <albertokatojr@gmail.com>
2020-03-23 01:16:27 -0300

    adding a table for ranking

--- src/views/Ranking.vue
+++ src/views/Ranking.vue
@@ -1,6 +1,33 @@
 <template>
   <div>
-    <h1>Ranking</h1>
+    <h1 class="title is-1 has-text-centered">Ranking</h1>
+    <table class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth">
+      <thead>
+        <tr>
+          <th>
+            <abbr title="User">User</abbr>
+          </th>
+          <th>Prize</th>
+          <th>Time</th>
+        </tr>
+      </thead>
+      <tfoot>
+        <tr>
+          <th>
+            <abbr title="User">User</abbr>
+          </th>
+          <th>Ranking</th>
+          <th>Time</th>
+        </tr>
+      </tfoot>
+      <tbody>
+        <tr>
+          <th>15</th>
+          <td>$1 MILLION</td>
+          <td>150</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
