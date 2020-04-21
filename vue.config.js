b02c7282b4ed38e650e23277a0c2f14266be1237
ASKJR <albertokatojr@gmail.com>
2020-04-20 21:11:53 -0300

    adding Preload key requests for styles.

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,30 @@
+const plugins = [];
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+if (process.env.NODE_ENV === 'development') {
+    plugins.push(new BundleAnalyzerPlugin());
+}
+
 module.exports = {
+    chainWebpack: config => {
+        config.plugin('preload').tap(options => {
+            options[0].as = (entry) => {
+                if (/\.css$/.test(entry)) return 'style';
+                if (/\.woff$/.test(entry)) return 'font';
+                if (/\.png$/.test(entry)) return 'image';
+                return 'script';
+            }
+            options[0].include = 'allAssets'
+            return options
+        })
+    },
     configureWebpack:{
-      performance: {
-        hints: false
-      },
       optimization: {
         splitChunks: {
           minSize: 10000,
           maxSize: 250000,
         }
-      }
+      },
+      plugins
     }
   }
