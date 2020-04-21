80032129285b958635ebc2e2c928adc67db24d10
ASKJR <albertokatojr@gmail.com>
2020-04-21 16:29:05 -0300

    removing material design icons

--- src/views/Ranking.vue
+++ src/views/Ranking.vue
@@ -28,7 +28,7 @@
         <section class="section">
           <div class="content has-text-grey has-text-centered">
             <p>
-              <b-icon icon="emoticon-sad" size="is-large"></b-icon>
+              <b-icon pack="fas" icon="frown" size="is-large"></b-icon>
             </p>
             <p>Nothing here.</p>
           </div>
